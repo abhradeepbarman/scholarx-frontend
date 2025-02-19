156709aed088cb108b8aaa2c4008d241246c7384
abhradeepbarman <abhradeep709@gmail.com>
2025-02-20 00:45:40 +0530

    fix: access token refresh bug fixed

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,6 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+    BaseQueryFn,
+    createApi,
+    FetchArgs,
+    fetchBaseQuery,
+    FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { config } from "@/config/config";
+import { deleteCredentials, setCredentials } from "@/features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: config.API_BASE_URL,
@@ -14,34 +21,36 @@ const baseQuery = fetchBaseQuery({
     },
 });
 
-// const baseQueryWithRefresh = async (args: any, api: any, extraOptions: any) => {
-//     let result = await baseQuery(args, api, extraOptions);
+const baseQueryWithRefresh: BaseQueryFn<
+    string | FetchArgs,
+    unknown,
+    FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
 
-//     if (result.error && result.error.status === 401) {
-//         // Attempt to refresh token
-//         const refreshResult = await baseQuery(
-//             { url: "/auth/refresh", method: "POST" },
-//             api,
-//             extraOptions
-//         );
+    if (result.error && result.error.status === 401) {
+        // Attempt to refresh token
+        const refreshResult = await baseQuery(
+            { url: "/auth/refresh", method: "POST" },
+            api,
+            extraOptions
+        );
 
-//         if (refreshResult.data) {
-//             // Store the new access token
-//             api.dispatch({
-//                 type: "auth/setAccessToken",
-//                 payload: refreshResult.data.access_token,
-//             });
+        if (refreshResult.data) {
+            api.dispatch(setCredentials(refreshResult.data));
 
-//             // Retry the original request with the new token
-//             result = await baseQuery(args, api, extraOptions);
-//         }
-//     }
+            // Retry the original request with the new token
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            deleteCredentials();
+        }
+    }
 
-//     return result;
-// };
+    return result;
+};
 
 export const apiSlice = createApi({
-    baseQuery,
+    baseQuery: baseQueryWithRefresh,
     refetchOnFocus: true,
     refetchOnReconnect: true,
     tagTypes: ["User"],
